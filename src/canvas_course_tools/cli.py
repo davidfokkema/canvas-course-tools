09e7363557950f1497b0ab59b18c7c8954930d9d
David Fokkema <davidfokkema@gmail.com>
2021-05-24 22:06:49 +0200

    Added show + add servers

--- src/canvas_course_tools/cli.py
+++ src/canvas_course_tools/cli.py
@@ -2,7 +2,8 @@ import pathlib
 
 import appdirs
 import click
-from rich import print
+from rich import print, box
+from rich.table import Table
 import toml
 
 from . import __version__
@@ -18,30 +19,85 @@ def cli():
 
 
 @cli.group()
-def apikey():
+def servers():
     pass
 
 
-@apikey.command("show")
-def show_apikey():
-    """Show the registered API key."""
+@servers.command("show")
+def show_servers():
+    """Show the registered servers and access tokens."""
     config = read_config()
     try:
-        key = config["canvas"]["apikey"]
+        servers = config["servers"]
     except KeyError:
-        print("[bold red]ERROR: API key not yet defined.[/bold red]")
-        print()
-        print("Use [green]canvas apikey set[/green]")
+        pass
     else:
-        print(f"API key: {key}")
+        table = Table(box=None)
+        table.add_column("Alias")
+        table.add_column("URL")
+        for alias, settings in servers.items():
+            table.add_row(alias, settings["url"])
+        print()
+        print(table)
+
+
+@servers.command("add")
+@click.argument("alias", type=str)
+@click.argument("url", type=str)
+@click.argument("token", type=str)
+@click.option(
+    "-f",
+    "--force",
+    type=bool,
+    default=False,
+    help="If alias already exists, force overwrite.",
+)
+def add_server(alias, url, token, force):
+    """Register an alias for a server with corresponding access token.
+
+    Example:
+
+        canvas servers add school http://school.example.com/ 123~secret
+    """
+    config = read_config()
+    servers = config.setdefault("servers", {})
+    if alias in servers and not force:
+        print(f"[bold red] Server alias {alias} already exists.[/bold red]")
+    else:
+        servers[alias] = {"url": url, "token": token}
+        write_config(config)
 
 
 def read_config():
     """Read configuration file."""
-    config_dir = pathlib.Path(appdirs.user_config_dir(APP_NAME))
-    config_path = config_dir / CONFIG_FILE
+    config_path = get_config_path()
     if config_path.is_file():
         with open(config_path) as f:
             return toml.load(f)
     else:
         return {}
+
+
+def write_config(config):
+    """Write configuration file.
+
+    Args:
+        config: a dictionary containing the configuration.
+    """
+    create_config_dir()
+    config_path = get_config_path()
+    with open(config_path, "w") as f:
+        toml.dump(config, f)
+
+
+def get_config_path():
+    """Get path of configuration file."""
+    config_dir = pathlib.Path(appdirs.user_config_dir(APP_NAME))
+    config_path = config_dir / CONFIG_FILE
+    return config_path
+
+
+def create_config_dir():
+    """Create configuration directory if necessary."""
+    config_path = get_config_path()
+    config_path.parent.mkdir(parents=True, exist_ok=True)
