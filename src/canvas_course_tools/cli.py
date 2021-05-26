1573422a6e076e8fded362ad945c8be62cd1f55d
David Fokkema <davidfokkema@gmail.com>
2021-05-26 21:02:02 +0200

    Added module docstring

--- src/canvas_course_tools/cli.py
+++ src/canvas_course_tools/cli.py
@@ -1,3 +1,11 @@
+"""Canvas course tools command-line application.
+
+This command-line application enables you to quickly perform a variety of tasks
+that would otherwise require you to log in to a Canvas site. Since using this
+tool requires no mouse-clicks at all, these tasks are performed much quicker and
+easier than throug the web interface.
+"""
+
 import pathlib
 
 import appdirs
