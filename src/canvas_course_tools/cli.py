764a54ef64e55e1c7a141565375f6b03a4c1d784
David Fokkema <davidfokkema@gmail.com>
2021-05-27 15:55:09 +0200

    Changed start_at time to academic year

--- src/canvas_course_tools/cli.py
+++ src/canvas_course_tools/cli.py
@@ -10,6 +10,7 @@ import pathlib
 
 import appdirs
 import click
+import dateutil.parser
 from rich import print, box
 from rich.table import Table
 import toml
@@ -112,10 +113,14 @@ def list_courses(alias):
         table.add_column("ID")
         table.add_column("Course Code")
         table.add_column("Name")
-        table.add_column("Start")
+        table.add_column("Year")
         for course in courses:
+            if course.start_at is not None:
+                academic_year = academic_year_from_time(course.start_at)
+            else:
+                academic_year = "Unknown"
             table.add_row(
-                str(course.id), course.course_code, course.name, course.start_at
+                str(course.id), course.course_code, course.name, academic_year
             )
         print()
         print(table)
@@ -154,3 +159,16 @@ def create_config_dir():
     """Create configuration directory if necessary."""
     config_path = get_config_path()
     config_path.parent.mkdir(parents=True, exist_ok=True)
+
+
+def academic_year_from_time(time):
+    """Return the academic year from a time string.
+
+    Args:
+        time: a string with a time in ISO format.
+    """
+    time = dateutil.parser.isoparse(time)
+    start_year = time.year
+    if time.month < 8:
+        start_year -= 1
+    return f"{start_year}-{start_year + 1}"
