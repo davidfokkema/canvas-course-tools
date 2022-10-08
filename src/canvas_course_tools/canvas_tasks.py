e39602ef4e6faf975636154a2fb3e571d3b65e7a
David Fokkema <davidfokkema@icloud.com>
2022-10-08 12:39:48 +0200

    Parse human names using nameparser

--- src/canvas_course_tools/canvas_tasks.py
+++ src/canvas_course_tools/canvas_tasks.py
@@ -4,6 +4,7 @@ from typing import Optional
 import dateutil.parser
 from canvasapi import Canvas
 from canvasapi.exceptions import Forbidden, InvalidAccessToken, ResourceDoesNotExist
+from nameparser import HumanName
 
 
 @dataclass
@@ -23,7 +24,16 @@ class Course:
 class Student:
     id: int
     name: str
-    sortable_name: str
+    sortable_name: Optional[str] = None
+    first_name: str = field(init=False)
+    last_name: str = field(init=False)
+
+    def __post_init__(self):
+        # Parse human names, allowing for underscores to group parts of the name
+        name = HumanName(self.name)
+        self.first_name = " ".join([name.first, name.middle]).rstrip()
+        self.last_name = name.last.replace("_", " ")
+        self.name = self.name.replace("_", " ")
 
 
 @dataclass
