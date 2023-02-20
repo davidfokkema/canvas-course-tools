e4bea678326f9cd09a04670b54598981536afaf2
David Fokkema <davidfokkema@icloud.com>
2023-02-20 19:49:33 +0100

    Use better TOML libraries

--- src/canvas_course_tools/configfile.py
+++ src/canvas_course_tools/configfile.py
@@ -1,7 +1,8 @@
 import pathlib
 
 import appdirs
-import toml
+import tomli
+import tomli_w
 
 
 APP_NAME = "canvas-course-tools"
@@ -12,25 +13,30 @@ def read_config():
     """Read configuration file."""
     config_path = get_config_path()
     if config_path.is_file():
-        with open(config_path) as f:
-            return toml.load(f)
+        try:
+            with open(config_path, "rb") as f:
+                return tomli.load(f)
+        except (tomli.TOMLDecodeError, UnicodeDecodeError):
+            # error parsing TOML
+            return {}
     else:
         return {}
 
 
 def write_config(config):
     """Write configuration file.
-
     Args:
         config: a dictionary containing the configuration.
     """
     create_config_dir()
     config_path = get_config_path()
-    toml_config = toml.dumps(config)
-    with open(config_path, "w") as f:
+    # make sure that TOML conversion works before opening file
+    tomli_w.dumps(config)
+    with open(config_path, "wb") as f:
         # separate TOML generation from writing to file, or an exception
         # generating TOML will result in an empty file
-        f.write(toml_config)
+        # correct TOML unicode handling requires writing bytes
+        tomli_w.dump(config, f)
 
 
 def get_config_path():
