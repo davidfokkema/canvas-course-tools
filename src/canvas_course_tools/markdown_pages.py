22bee5dd90be5491aa4505f350857efcb1d59559
David Fokkema <davidfokkema@icloud.com>
2025-06-17 21:21:42 +0200

    Get slug from page title (h1), remove from html

--- src/canvas_course_tools/markdown_pages.py
+++ src/canvas_course_tools/markdown_pages.py
@@ -3,8 +3,21 @@
 import pathlib
 
 import mistune
+from bs4 import BeautifulSoup
+from slugify import slugify
+
+from canvas_course_tools.utils import find_course
 
 markdown = mistune.create_markdown(
     plugins=["math", "footnotes", "superscript"],
 )
-print(markdown((pathlib.Path(__file__).parent / "testdoc.md").read_text()))
+html = markdown((pathlib.Path(__file__).parent / "testdoc.md").read_text())
+
+canvas, course = find_course("test")
+
+soup = BeautifulSoup(html, "html.parser")
+h1 = soup.find("h1")
+slug = slugify(h1.text)
+h1.decompose()
+
+print(soup)
