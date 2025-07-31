9ac857b90a152a1d06a2f44bb75d7bc65f15927d
David Fokkema <davidfokkema@icloud.com>
2025-07-31 20:44:44 +0200

    Upload a markdown file

--- scripts/upload_page.py
+++ scripts/upload_page.py
@@ -1,24 +1,9 @@
+from pathlib import Path
+
 from canvas_course_tools.utils import find_course
 
 canvas, course = find_course("test")
 page = canvas.upload_markdown_page(
-    course,
-    content="""\
-# HA fancy new page
-
-This is a new fancy page.
-
-## Section heading
-
-### Subsection
-
-```python
-print("Hello, world!")
-```
-
-$$
-E = mc^2
-$$
-""",
+    course, content=(Path(__file__).parent / "testdoc.md").read_text()
 )
 print(page)
